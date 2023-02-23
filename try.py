998a4569daebf89198cb02e22df733c606887827
shubh204 <shubhamg22@iiserb.ac.in>
2023-02-24 01:19:02 +0530

    changes to html and py files

--- try.py
+++ try.py
@@ -5,14 +5,31 @@ abc="https://www.thenakedscientists.com/articles/science-news/physics"
 req = requests.get(abc)
 soup = BeautifulSoup(req.text , "html.parser")
 #print(soup.prettify)
-#x= soup.find_all( class_="panel-pane pane-views-panes pane-featured-podcast-panel-pane-8")
+htcon= soup.find_all(class_="col-xs-12 col-lg-4")
 x= soup.find("a")
 y= soup.a.prettify
 
 #print(x)
+#print(htcon.get('href'))
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
+for link in soup.find_all("h3" , class_="field-content"):
+     for i in link:
+
+        print(i.get('href'))
+    
+
+   #lis=link.get('href')
+   #print(lis)
 
 #print(req.content)
+
+"""
+htcon=str(htcon)
+openfile=open('htm.html','r+')
+openfile.write(htcon)
+
+openfile.close()
+"""
+
+
 
