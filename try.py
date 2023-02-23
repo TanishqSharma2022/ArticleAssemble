3bb29cb4426f77f73564b7105f115231b2b63572
shubh204 <shubhamg22@iiserb.ac.in>
2023-02-23 23:21:42 +0530

    initial

---
+++ try.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+
+abc="https://www.thenakedscientists.com/articles/science-news/physics"
+req = requests.get(abc)
+soup = BeautifulSoup(req.text , "html.parser")
+#print(soup.prettify)
+#x= soup.find_all( class_="panel-pane pane-views-panes pane-featured-podcast-panel-pane-8")
+x= soup.find("a")
+y= soup.a.prettify
+
+#print(x)
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
+
+#print(req.content)
+
