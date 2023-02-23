856370c554c6f3d8d98afaebf0adca6c15afa2d8
TanishqSharma2022 <tanishq22@iiserb.ac.in>
2023-02-24 04:12:17 +0530

    Added Features

--- try.py
+++ try.py
@@ -4,29 +4,49 @@ import requests
 abc="https://www.thenakedscientists.com/articles/science-news/physics"
 req = requests.get(abc)
 soup = BeautifulSoup(req.text , "html.parser")
-#print(soup.prettify)
-#x= soup.find_all( class_="panel-pane pane-views-panes pane-featured-podcast-panel-pane-8")
-x= soup.find("a")
-y= soup.a.prettify
-
-#print(x)
-
-for link in soup.find_all('a'):
-    print(link.get('href'))
-
-#print(req.content)
 
 
+
+links = []
+headlines = []
+for classes in soup.find_all('h3', class_="field-content"):
+    for x in classes:
+        links.append("https://www.thenakedscientists.com"+x.get('href'))
+        headlines.append(x.contents)
+
+# EXTRACTING IMAGE LINKS
+"""
+for imgs in soup.find_all('div', class_="background-image-formatter"):
+    print(imgs.get('style'))
 """
 
-Transporting links to json to javascript:
+
+body = []
+
+
+
+for link in links:
+    scraplink = str(link)
+    bodyreq = requests.get(scraplink)
+    bodysoup = BeautifulSoup(bodyreq.text, "html.parser")
+    ins_tag = bodysoup.ins
+    ins_tag.decompose()
+
+    for x in bodysoup.find_all('div', class_="node-body"):
+        for i in x.children:
+            body.append(str(i))
+        
+    #print(body)
+
+#Transporting links to json to javascript:
 
 import json
 import sys
 
 sys.stdout = open('links.js', 'w')
 
-jsonobj = json.dumps(l)
+jsonobj = json.dumps(links)
+bodyobj = json.dumps(body)
 
-print("Links = '{}'".format(jsonobj))
-"""
+print("var Links = {}".format(jsonobj))
+print("var BodyContent = {}".format(bodyobj))
